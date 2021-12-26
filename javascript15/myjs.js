be6babc23cd4b7e8794d9a2470d655c3b4b984e7
範雷雷 <95039519+leileifan166@users.noreply.github.com>
2021-12-26 09:42:36 +0900

    Add files via upload

---
+++ javascript15/myjs.js
@@ -0,0 +1,35 @@
+document.write('<pre>');
+document.write('中文名: 沙奎尔・奥尼尔\n');
+document.write("英文名: shaquille o'Neal\n");
+document.write('英文名: shaquille o\'Neal\n');
+document.write('别名: 大鲨鱼');
+document.write('</pre>');
+document.write('E:\\JavaScript\\Code\\Try<br>');
+document.write('天之道，损有余而补不足<br>'+ '是故虚胜实，不足而胜有余<br>');
+let n = 1;
+let m = 2;
+if (n==1)
+    m=m+1;
+else
+    n=n+1;
+console.log(m);
+console.log(n);
+console.log(m,n);
+console.log(m-n);
+console.log(m+n);
+let number6 = null;
+console.log(typeof(number6));
+let number7;
+console.log(typeof(number7));
+var variablename;
+var username;
+var a,b,c;
+//var lesson='零基础学习javascript';
+var lesson;
+lesson='零基础学javascript';
+str = '这是一个未声明的变量';
+document.write(str);
+
+
+
+
